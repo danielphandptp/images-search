2290a2db9bac34ece1fe9f1664d617c64dfb4251
Daniel Phan <107604542+danielphandptp@users.noreply.github.com>
2022-06-29 00:07:17 -0400

    added scroll bar and extra functionalities for the UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
-import SearchBox from './SearchBox'
-import { robots } from "./robots";
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
+import { robots } from './robots';
+import './App.css';
 
-const App = () => {
-	return (
-		<div className="tc">
-			<h1> Cat Friends </h1>
-			<SearchBox />
-			<CardList robots={robots}/>
-		</div>
-	)
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			robots: [],
+			searchField: ''
+		}
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchField: event.target.value })
+	}
+	
+	componentDidMount() {
+		// fetch('https://jsonplaceholder.typicode.com/users')
+		// 	.then(response => {
+		// 		return response.json();
+		// 	})
+		// 	.then(users => {
+		// 		this.setState({ robots: users})
+		// 	})
+		this.setState({ robots: robots })
+	}
+
+	render() {
+		const filteredCats = this.state.robots.filter(robots => {
+			return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+		})
+		if (this.state.robots.length === 0) {
+			return <h1>Loading...</h1>;
+		} else {
+			return (
+				<div className="tc">
+					<h1 className="f1"> Kitten Playground </h1>
+					<SearchBox searchField={"Search Cats"} searchChange={this.onSearchChange} />
+					<Scroll>
+						<CardList robots={filteredCats}/>
+					</Scroll>
+				</div>
+			)
+		}
+	}
 }
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ({ searchField, searchChange}) => {
 	return (
-		<div className="pa2">
+		<div className="pa1">
 			<input 
-				className="pa2 ba b--green bg-lightest-blue" 
-				type="search" name="search cats" value=""
+				className="f4 br3 pa3 ba b--black bg-lightest-gray" 
+				type="search" 
+				placeholder="Search Cats..."
+				onChange={searchChange}
 			/>
 		</div>
 	);
